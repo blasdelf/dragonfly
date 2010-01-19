8fbb34c4094792eba30bb3d17f455d506e028d45
Fred Blasdel <blasdelf@gmail.com>
2010-01-19 12:12:12 -0800

    Have FileCommandAnalyser write to file(1)'s stdin
    Avoid creating a temp file for every request.

--- lib/dragonfly/analysis/file_command_analyser.rb
+++ lib/dragonfly/analysis/file_command_analyser.rb
@@ -8,8 +8,11 @@ module Dragonfly
       configurable_attr :file_command do `which file`.chomp end
       
       def mime_type(temp_object)
-        output = `#{file_command} -b --mime #{temp_object.path}`
-        output.split(';').first
+        IO.popen("#{file_command} -b --mime-type -", mode='r+') do |io|        
+          io.write_nonblock temp_object.data
+          io.close_write
+          io.read         
+        end
       end
       
     end
